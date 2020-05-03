5c2d1732df589eab794a3158c60e22cfbd660932
GouravSaini125 <rahul777singh000@gmail.com>
2020-05-03 20:09:40 +0530

    Updates

---
+++ precache-manifest.91bcde0123223b6a8ca0e74d263f3094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf8f73115f7332ad0a523719250ce54",
+    "url": "/apigo/index.html"
+  },
+  {
+    "revision": "6c9e05713525adbf554c",
+    "url": "/apigo/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1b02c09d77b9592872e5",
+    "url": "/apigo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c9e05713525adbf554c",
+    "url": "/apigo/static/js/2.0345811b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/apigo/static/js/2.0345811b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b02c09d77b9592872e5",
+    "url": "/apigo/static/js/main.37cae7cc.chunk.js"
+  },
+  {
+    "revision": "a5b120be51fdc8ced38e",
+    "url": "/apigo/static/js/runtime-main.b1c5d4d5.js"
+  }
+]);
