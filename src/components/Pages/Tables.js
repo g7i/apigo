498f224203e59d94800fb9626ece4f030ca6ec13
GouravSaini125 <rahul777singh000@gmail.com>
2020-05-03 21:00:59 +0530

    heroku configs

--- src/components/Pages/Tables.js
+++ src/components/Pages/Tables.js
@@ -58,7 +58,7 @@ export default function Tables(props) {
             fields : fieldsObj
         };
 
-        const configs = {
+        const config = {
             headers: {
                 "Access-Control-Allow-Origin": "*",
             }
